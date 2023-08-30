dc99bc18c6570b9230353ea980dc72c7254ec5af
Felix9908 <frfyfelix@gmail.com>
2023-08-29 22:35:28 -0400

    Añadiendo jwt y ruta del login

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
-const express = require('express');
-const cors = require('cors');
-const mysql = require('mysql2/promise');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2/promise");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+const keys = require("./settings/keys");
+const secret_key = keys.key;
 
 // Configuración de la base de datos
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'tu_contraseña',
-  database: 'acortador_db'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "acortador",
 });
 
+async function checkDatabaseConnection() {
+  try {
+    await db.execute("SELECT 1");
+    console.log("Conexión exitosa a la base de datos");
+  } catch (error) {
+    console.error("Error al conectar a la base de datos:", error);
+    process.exit(1); // Salir con código de error si no hay conexión
+  }
+}
+
 // Ruta para generar un enlace aleatorio
-app.post('/generar-enlace', async (req, res) => {
+app.post("/generar-enlace", async (req, res) => {
   const originalUrl = req.body.url;
 
   // Generar código aleatorio para el enlace
@@ -25,13 +37,54 @@ app.post('/generar-enlace', async (req, res) => {
   // Guardar el enlace en la base de datos
   try {
     const [result] = await db.execute(
-      'INSERT INTO enlaces (codigo, url_original) VALUES (?, ?)',
+      "INSERT INTO enlaces (codigo, url_original) VALUES (?, ?)",
       [enlaceCodigo, originalUrl]
     );
     res.json({ success: true, enlaceCodigo });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, error: 'Error al generar el enlace.' });
+    res
+      .status(500)
+      .json({ success: false, error: "Error al generar el enlace." });
+  }
+});
+
+// Ruta para el inicio de sesión
+app.post("/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  try {
+    const [rows] = await db.execute(
+      "SELECT * FROM usuarios WHERE user = ? AND password = ?",
+      [username, password]
+    );
+
+    if (rows.length > 0) {
+      const payload = {
+        check: true,
+        data: rows,
+      };
+      jwt.sign(payload, "secret_key", (err, token) => {
+        if (err) {
+          res.status(400).send(err);
+        } else {
+          res.send({
+            msg: "AUTEMTICACION EXITOSA",
+            token: token,
+            userData: rows,
+          });
+        }
+      });
+    } else {
+      // Credenciales incorrectas
+      res
+        .status(401)
+        .json({ success: false, message: "Credenciales incorrectas." });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Error en el servidor." });
   }
 });
 

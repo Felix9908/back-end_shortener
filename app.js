bfd2af6a8103ed8242f7f9dc1c47a220e75ab365
Felix9908 <frfyfelix@gmail.com>
2023-09-27 23:12:56 -0400

    add funcion de crear Cuenta de usuarios

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ const secret_key = keys.key;
 
 // Configuración de la base de datos
 const db = mysql.createPool({
-    host: "sql9.freemysqlhosting.net",
-    user: "sql9648511",
-    password: "CaTi7kel3Y",
-    database: "sql9648511",
+  host: "sql9.freemysqlhosting.net",
+  user: "sql9648511",
+  password: "CaTi7kel3Y",
+  database: "sql9648511",
 });
 
 async function checkDatabaseConnection() {
@@ -88,6 +88,52 @@ app.post("/login", async (req, res) => {
   }
 });
 
+//Ruta para crear cuenta
+app.post("/createAccount", async (req, res) => {
+  const {
+    user,
+    password,
+    email,
+    privUser,
+    formadepago,
+    direccionParticular,
+    telephoneNumber,
+    ciudad,
+    npmbreCompleto,
+    estado,
+    pais,
+  } = req.body;
+
+  if (!user || !password || !email || !privUser) {
+    res.status(400).send("No se recibieron todos los datos requeridos.");
+    return;
+  }
+
+  const sql =
+    "INSERT INTO usuarios (user, password, priv, formadepago, direccionParticular, correo, telephoneNumber, ciudad, nombreCompleto, estado, pais) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+  const values = [
+    user,
+    password,
+    privUser,
+    formadepago || null, // Agregar valores predeterminados si son null
+    direccionParticular || null,
+    email,
+    telephoneNumber || null,
+    ciudad || null,
+    npmbreCompleto || null,
+    estado || null,
+    pais || null,
+  ];
+
+  try {
+    await db.execute(sql, values);
+    res.status(200).send("Usuario registrado exitosamente");
+  } catch (error) {
+    console.error("Error al insertar datos en la tabla SQL:", error);
+    res.status(500).send("Error al insertar datos en la tabla SQL");
+  }
+});
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
